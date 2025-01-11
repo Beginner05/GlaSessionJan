db2f179cab0c6a2cec162a1fbe4a7c189338a546
Hitesh Malhotra <hiteshmalhotra0018@gmail.com>
2025-01-11 18:33:45 +0530

    first Commit

---
+++ Lec2/BoardPath.java
@@ -0,0 +1,10 @@
+package Lec2;
+
+public class BoardPath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GLA_SessionJan {
+}
