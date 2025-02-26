9aafebe13eeaf2419a51548e848cdb2ed13ec074
Hitesh Malhotra <hiteshmalhotra0018@gmail.com>
2025-02-26 19:08:38 +0530

    merge sort

--- Backtracking/mergeSort.java
+++ Backtracking/mergeSort.java
@@ -3,21 +3,65 @@ package Backtracking;
 public class mergeSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int arr[] = { 1, 2, 32, 43, 2, 4, 0 };
-		sol(arr, 0, arr.length - 1);
 
+		int arr[] = { 4, 3, 2, 43, 0, 1, -8 };
+		int res[]=sort(arr, 0, arr.length - 1);
+	for(int i=0;i<res.length;i++)
+	{
+		System.out.print(res[i]+" ");
+	}
 	}
 
-	public static int[] sol(int arr[], int lo, int hi) {
+	public static int[] sort(int arr[], int lo, int hi) {
 		if (lo == hi) {
 			int res[] = new int[1];
 			res[0] = arr[lo];
 			return res;
 		}
 		int mid = (lo + hi) / 2;
-		int left[] = sol(arr, lo, mid);
-		int right[] = sol(arr, mid + 1, hi);
+		int left[] = sort(arr, lo, mid);
+		int right[] = sort(arr, mid + 1, hi);
 		return merge(left, right);
+	}
+
+	public static int[] merge(int left[], int right[]) {
+		int res[] = new int[left.length + right.length];
+		int i = 0;
+		int j = 0;
+		int k = 0;
+//		i track rkh rha h left array ka;
+//		j track rkh rha h right array ka;
+//		k track rakh rha h res array ka;
+		while (i < left.length && j < right.length) {
+			if (left[i] < right[j]) {
+				res[k] = left[i];
+				k++;
+				i++;
+			} else if (left[i] > right[j]) {
+				res[k] = right[j];
+				k++;
+				j++;
+			}
+		}
+//		ho skta h j end tk phoch gya ho
+//		agr, j end pr phoch gya mtlb i kch element baaki h
+
+//		ho skta h i end tk phoch gya ho
+//		agr, i end pr phoch gya mtlb j kch element baaki h
+while(i<left.length)
+{
+	res[k]=left[i];
+	i++;
+	k++;
+}
+while(j<right.length)
+{
+	res[k]=right[j];
+	j++;
+	k++;
+}
+		
+		return res;
+
 	}
 }
--- BinarySearch/Check.java
+++ BinarySearch/Check.java
@@ -4,68 +4,34 @@ public class Check {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		sol("III", 0, '*', "", new int[10]);
+		String ans = sol("IIIDIDDD", 0, '*', "", new int[10]);
+		System.out.println(ans);
 	}
 
-	public static void sol(String str, int num, char prev, String ans, int arr[]) {
-		if (str.length() == 0) {
-			if (prev == 'D') {
+	public void sol(String str)
+    {
+        HashMap<Integer,ArrayList<Integer>map=new HashMap();
+        int depth=0;
+for(int i=0;i<str.length();)
+{
+while(str.charAt(i)=='-')
+{
+    depth++;
+    i++;
+}
+if(map.containsKey(depth))
+{
+map.get(depth).add(str.charAt(i)-'0');
+}
+else{
+map.put(depth,new ArrayList());
+map.get(depth).add(str.charAt(i)-'0');
+}
+depth=0;
+i++;
+}
 
-				for (int i = 1; i < 10; i++) {
-					if (i < num && arr[i] == 0) {
-						arr[i] = i;
-						System.out.println(ans + i);
-//							sol(str.substring(1),i,str.charAt(0),ans+i,arr);
-						arr[i] = 0;
-						return;
-					}
-				}
-			}
 
-			else {
 
-				if (prev == 'I') {
-					for (int i = 1; i < 10; i++) {
-						if (num < i && arr[i] == 0) {
-							arr[i] = i;
-//						sol(str.substring(i),i,str.charAt(0),ans+i,arr);
-							System.out.println(ans + i);
-
-							arr[i] = 0;
-							return;
-						}
-					}
-				}
-			}
-//			System.out.println(ans);
-			return;
-		}
-		if (ans.length() == 0) {
-			for (int i = 1; i < 10; i++) {
-				arr[i] = i;
-				sol(str.substring(1), i, str.charAt(0), ans + i, arr);
-				arr[i] = 0;
-			}
-		} else {
-			if (prev == 'D') {
-				for (int i = 1; i < 10; i++) {
-					if (i < num && arr[i] == 0) {
-						arr[i] = i;
-						sol(str.substring(1), i, str.charAt(0), ans + i, arr);
-						arr[i] = 0;
-					}
-				}
-			} else if (prev == 'I') {
-				for (int i = 1; i < 10; i++) {
-					if (num < i && arr[i] == 0) {
-						arr[i] = i;
-						sol(str.substring(i), i, str.charAt(0), ans + i, arr);
-						arr[i] = 0;
-					}
-				}
-			}
-		}
-
-	}
-
+    }
 }
